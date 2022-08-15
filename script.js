0e3724d3abe4a02a518c406491b6fb755cbdc81e
R-Saunders <richsaunders@protonmail.com>
2022-08-15 18:36:46 +0100

    create element, create text node

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function clicked() {
-  console.log('You\'re Awesome!');
-
-  
+  // Console Log
+  console.log('You\'re simply the best!');
+  // Create a `p` element
+  const creating = document.createElement('p');
+  console.log(creating);
+  // Create a text node
+  const createContent = document.createTextNode('Who\'s awesome? You\'re awesome!');
+  console.log(createContent);
 
 }
